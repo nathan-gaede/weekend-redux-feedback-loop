bd8dc7ff2b1d8dc71f68ba04355491cfb5a2058b
Nathan Gaede <n.d.gaede@gmail.com>
2022-09-20 10:11:46 -0500

    Fixed comment not posting to table.

--- server/Routes/summary.router.js
+++ server/Routes/summary.router.js
@@ -7,8 +7,8 @@ const pool = require('../modules/pool');
 router.post('/', (req, res) => {
 console.log('in router.post', req.body);
 let queryText = 'INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);';
-let {feeling, understanding, support, comments} = req.body;
-pool.query(queryText, [feeling, understanding, support, comments])
+let {feeling, understanding, support, comment} = req.body;
+pool.query(queryText, [feeling, understanding, support, comment])
 .then((result) => {
     res.sendStatus(200);
 }).catch((error) => {
